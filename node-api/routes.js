446d76b1ec2d79fd95667adc87106080c46366bb
[gusflopes] <[gustavo@lscont.com.br]>
2019-09-11 13:42:17 -0400

    Rollback e reorganizando arquivos.

--- node-api/routes.js
+++ node-api/routes.js
@@ -6,7 +6,6 @@
 
 
 module.exports = function(app) {
-    app.use('api/v1/auths'), require('./')
     app.use('/api/v1/auths', require('./api/auths'));
     app.use('/api/v1/vendors', require('./api/vendors'));
 };
---
+++ vendor-crud/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './_reducers';
+//A linha segundo o tutorial está comentada
+import { register } from './serviceWorker';
+//import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+    <Provider store={store}>
+       <App />
+    </Provider>
+, document.getElementById('root'));
+
+register();
+//registerServiceWorker();
